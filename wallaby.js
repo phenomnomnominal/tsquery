8af0ccb02cfbb0b367047c0978337dafa3f906ef
Uri Shaked <uri@urishaked.com>
2018-05-14 16:45:30 +0300

    (TOOLING 🔨) - Wallaby.js configuration

---
+++ wallaby.js
@@ -0,0 +1,9 @@
+module.exports = function(wallaby) {
+	return {
+		files: ['src/**/*.ts', { pattern: 'test/fixtures/**/*.ts', instrument: false }],
+		tests: ['test/**/*.spec.ts'],
+		env: {
+			type: 'node',
+		},
+	};
+};
