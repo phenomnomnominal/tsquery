e08e65692a1d9e9b3c5fe23dbd2876e2aa76cba7
Vincent Ogloblinsky <vincent.ogloblinsky@gmail.com>
2018-11-07 15:22:37 +0100

    fixes(chore): correct use of ScriptKind enum

--- src/ast.ts
+++ src/ast.ts
@@ -1,7 +1,6 @@
 // Dependencies:
-import { createSourceFile, ScriptTarget, SourceFile } from 'typescript';
+import { createSourceFile, ScriptKind, ScriptTarget, SourceFile } from 'typescript';
 
-// ScriptKind values : https://github.com/Microsoft/TypeScript/blob/master/src/compiler/types.ts#L4626
-export function createAST (source: string, fileName?: string, scriptKind?: number): SourceFile {
+export function createAST (source: string, fileName?: string, scriptKind?: ScriptKind): SourceFile {
     return createSourceFile(fileName || '', source, ScriptTarget.Latest, true, scriptKind);
 }
--- test/ast.spec.ts
+++ test/ast.spec.ts
@@ -1,3 +1,5 @@
+import { ScriptKind } from 'typescript';
+
 // Test Utilities:
 import { expect } from './index';
 
@@ -10,7 +12,7 @@ import { tsquery } from '../src/index';
 describe('tsquery:', () => {
     describe('tsquery - jsx:', () => {
         it('should get a correct AST from jsx code', () => {
-            const ast = tsquery.ast(simpleJsxCode, '', 2);
+            const ast = tsquery.ast(simpleJsxCode, '', ScriptKind.JSX);
 
             expect(ast.statements.length).to.equal(3);
         });
