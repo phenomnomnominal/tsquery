4a345be5b20a1f5a215d6b5b1ba9262cc9ae97bf
Run1t <reunanln@gmail.com>
2018-11-17 16:13:15 +0100

    Fix linting

--- src/matchers/root.ts
+++ src/matchers/root.ts
@@ -1,4 +1,4 @@
-import { Node } from "typescript";
+import { Node } from 'typescript';
 
 export function  root ({}: any, {}: any, ancestry: Array<Node>): boolean {
     return ancestry.length === 0;
--- src/matchers/scope.ts
+++ src/matchers/scope.ts
@@ -1,4 +1,4 @@
-import { Node } from "typescript";
+import { Node } from 'typescript';
 
 export function  scope ({}: any, {}: any, ancestry: Array<Node>): boolean {
     return ancestry.length === 0;
