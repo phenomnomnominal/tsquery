f0630106cd2f19ca910e5680f350a9b6ea74a231
Run1t <reunanln@gmail.com>
2018-11-17 17:28:27 +0100

    Fix type

--- src/tsquery-types.ts
+++ src/tsquery-types.ts
@@ -8,7 +8,7 @@ export type TSQueryApi = {
    <T extends Node = Node> (ast: string | Node, selector: string, options?: TSQueryOptions): Array<T>;
    ast (source: string, fileName?: string, scriptKind?: ScriptKind): SourceFile;
    map (ast: SourceFile, selector: string, nodeTransformer: TSQueryNodeTransformer, options?: TSQueryOptions): SourceFile;
-   match <T extends Node = Node> (ast: Node, selector: TSQuerySelectorNode, options?: TSQueryOptions): Array<T>;
+   match <T extends Node = Node> (ast: Node, selector: TSQuerySelectorNode, node: Node, options?: TSQueryOptions): Array<T>;
    parse (selector: string, options?: TSQueryOptions): TSQuerySelectorNode;
    project (configFilePath: string): Array<SourceFile>;
    query <T extends Node = Node> (ast: string | Node, selector: string, options?: TSQueryOptions): Array<T>;
