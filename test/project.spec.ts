2b283302201cfab2df107b08eb740374dafb89f2
Craig Spence <craigspence0@gmail.com>
2022-04-22 22:34:30 +0200

    Remove last chai code

--- test/project.spec.ts
+++ test/project.spec.ts
@@ -6,19 +6,19 @@ describe('tsquery:', () => {
     it('should process a tsconfig.json file', () => {
       const files = tsquery.project('./tsconfig.json');
 
-      expect(files.length).toEqual(171);
+      expect(files.length).toEqual(170);
     });
 
     it('should get the file paths from a tsconfig.json file', () => {
       const filePaths = tsquery.projectFiles('./tsconfig.json');
 
-      expect(filePaths.length).toEqual(57);
+      expect(filePaths.length).toEqual(56);
     });
 
     it('should find a tsconfig.json file in a directory', () => {
       const files = tsquery.project('./');
 
-      expect(files.length).toEqual(171);
+      expect(files.length).toEqual(170);
     });
 
     it(`should handle when a path doesn't exist`, () => {
