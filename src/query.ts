f44fe496a6cffb4ff883e8000c443d7152500637
wongchichong <wongchichong@gmail.com>
2023-07-06 10:22:07 +0800

    fix space

--- src/query.ts
+++ src/query.ts
@@ -5,9 +5,9 @@ import { match } from './match';
 import { parse } from './parse';
 import { TSQueryOptions } from './tsquery-types';
 
-export function query<T extends Node = Node>(ast: string | Node, selector: string, options: TSQueryOptions = {}): Array<T> {
-  if (typeof ast === 'string') {
-    ast = createAST(ast, undefined, options.scriptKind ?? ScriptKind.Unknown);
-  }
-  return match<T>(ast, parse(selector), options);
+export function query<T extends Node = Node> (ast: string | Node, selector: string, options: TSQueryOptions = {}): Array<T> {
+    if (typeof ast === 'string') {
+        ast = createAST(ast, undefined, options.scriptKind ?? ScriptKind.Unknown);
+    }
+    return match<T>(ast, parse(selector), options);
 }
