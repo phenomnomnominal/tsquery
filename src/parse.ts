6e02e54e427725b36c6724f5487c0c889ed04dc4
wongchichong <wongchichong@gmail.com>
2023-07-06 10:15:14 +0800

    fix space

--- src/parse.ts
+++ src/parse.ts
@@ -1,44 +1,44 @@
 // Dependencies:
-import esquery from 'esquery';
-import { SyntaxKind } from 'typescript';
-import { TSQuerySelectorNode } from './tsquery-types';
+import esquery from 'esquery'
+import { SyntaxKind } from 'typescript'
+import { TSQuerySelectorNode } from './tsquery-types'
 
 // Constants:
-const IDENTIFIER_QUERY = 'identifier';
+const IDENTIFIER_QUERY = 'identifier'
 
 export function parse(selector: string): TSQuerySelectorNode {
-  const cleanSelector = stripComments(stripNewLines(selector));
-  return validateParse(esquery.parse(cleanSelector));
+    const cleanSelector = stripComments(stripNewLines(selector))
+    return validateParse(esquery.parse(cleanSelector))
 }
 
 function stripComments(selector: string): string {
-  return selector.replace(/\/\*[\w\W]*\*\//g, '');
+    return selector.replace(/\/\*[\w\W]*\*\//g, '')
 }
 
 function stripNewLines(selector: string): string {
-  return selector.replace(/\n/g, '');
+    return selector.replace(/\n/g, '')
 }
 
 function validateParse(selector: TSQuerySelectorNode): TSQuerySelectorNode {
-  if (!selector) {
-    return selector;
-  }
-
-  if (selector.selectors) {
-    selector.selectors.map(validateParse);
-  }
-  if (selector.left) {
-    validateParse(selector.left);
-  }
-  if (selector.right) {
-    validateParse(selector.right);
-  }
-
-  if ((selector.type as string) === IDENTIFIER_QUERY) {
-    if (SyntaxKind[selector.value as any] == null) {
-      throw SyntaxError(`"${selector.value}" is not a valid TypeScript Node kind.`);
+    if (!selector) {
+        return selector
     }
-  }
 
-  return selector;
+    if (selector.selectors) {
+        selector.selectors.map(validateParse)
+    }
+    if (selector.left) {
+        validateParse(selector.left)
+    }
+    if (selector.right) {
+        validateParse(selector.right)
+    }
+
+    if ((selector.type as string) === IDENTIFIER_QUERY) {
+        if (SyntaxKind[selector.value as any] == null) {
+            throw SyntaxError(`"${selector.value}" is not a valid TypeScript Node kind.`)
+        }
+    }
+
+    return selector
 }
